fd3ba843f52e0b29ad30974b6d482b5c8ca2118e
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-04-02 16:47:53 -0400

    commented out sessions controller

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
+app.use(express.static('public'));
 app.use(express.json());
 
 const petController = require('./controllers/pet-controller.js');
 app.use('/pet-a-gram', petController);
 
-const sessionController = require('./controllers/pet-sessions-controller.js');
-app.use('/pet-a-gram', sessionController);
+
+//Comment code back in once sessions code has been established!
+// const sessionController = require('./controllers/pet-sessions-controller.js');
+// app.use('/sessions', sessionController);
 
 mongoose.connect('mongodb://localhost:27017/petagram');
 mongoose.connection.once('open', () => {
