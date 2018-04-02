4aa87c98f48406c9dc1a6dd692d0133854edc989
Shaun Savage <swansavage@gmail.com>
2018-04-02 19:45:13 -0400

    can get/create/edit/delete Pet Profiles on home page

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,15 @@
 const app = angular.module('petApp', []);
 
 
-app.controller('MyController', ['$http', function($http){
+app.controller('mainController', ['$http', function($http){
+
+    // empty array to store pets in
     this.allPets = [];
+
+    // empty object to add data from form submission into
+    this.formData = {};
+
+    this.indexOfEditFormToShow = null;
 
     this.getPets = () => {
         $http({
@@ -10,6 +17,54 @@ app.controller('MyController', ['$http', function($http){
             url:'/pet-a-gram'
         }).then((response) => {
             this.allPets = response.data
-        })
+        }, error => {
+            console.error(error);
+        }).catch(err => console.error('Catch ', err))
+    } // end getPets();
+
+    this.createPet = ()=> {
+        $http({
+            method: 'POST',
+            url:'/pet-a-gram',
+            data: this.formData
+        }).then( (res)=> {
+            console.log(res);
+            this.formData = {};
+            this.allPets.push(res.data);
+        }, error => {
+            console.error(error)
+        }).catch(err => console.error('Catch ', err));
+    } // end createPet();
+
+    this.editPet = ( pet ) => {
+        $http({
+            method: 'PUT',
+            url: '/pet-a-gram/' + pet._id,
+            data: {
+                name: this.editedName,
+                species: this.editedSpecies,
+                profileImg: this.editedProfileImg,
+                description: this.editedDescription
+            }
+        }).then( (res)=> {
+            console.log(res.data);
+            this.indexOfEditFormToShow = null;
+            this.getPets();
+        }, erorr => {
+            console.error(error)
+        }).catch(err => console.error('Catch '. err));
+    } // end editPet();
+
+    this.deletePet = ( id ) => {
+        $http({
+            method: 'DELETE',
+            url: '/pet-a-gram/' + id
+        }).then( (res)=>{
+            const removeByIndex = this.allPets.findIndex(pet => pet._id === id);
+            this.allPets.splice(removeByIndex, 1);
+        }, error => {
+            console.error(error);
+        }).catch(err => console.error('Catch ', err))
     }
-}])
+    this.getPets(); // <---- Load immediately on page load.
+}]) // end mainController
